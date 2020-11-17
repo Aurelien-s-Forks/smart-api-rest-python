f3f121e74d3b77d73dc64a204cbb82c2e4f3ffe9
Aurélien Riche <contact@aurelienriche.com>
2020-11-17 12:19:14 +0100

    Add data

--- routes/pipeline_pressure.py
+++ routes/pipeline_pressure.py
@@ -1,4 +1,4 @@
-from flask import json
+from flask import jsonify
 from datetime import datetime
 import random
 from . import routes
@@ -6,4 +6,9 @@ from . import routes
 
 @routes.route('/api/pipeline-pressure/<room_id>', methods=['GET'])
 def get_pipeline_pressure(room_id=0):
-    return "Hello pipeline"
+    room_data = {
+        'room_id': room_id,
+        'pipeline_pressure': random.randint(1, 5),
+        'date': datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    }
+    return jsonify(room_data)
--- routes/temperature.py
+++ routes/temperature.py
@@ -1,4 +1,4 @@
-from flask import json
+from flask import jsonify
 from datetime import datetime
 import random
 from . import routes
@@ -6,4 +6,9 @@ from . import routes
 
 @routes.route('/api/temperature/<room_id>', methods=['GET'])
 def get_temperature(room_id=0):
-    return "Hello temperature"
+    room_data = {
+        'room_id': room_id,
+        'temperature': random.randint(15, 22),
+        'date': datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    }
+    return jsonify(room_data)
