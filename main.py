ab3b5191e9dd7e068b837e57fe6c894c5434a496
Aurélien Riche <contact@aurelienriche.com>
2020-11-17 11:28:32 +0100

    Add Pipeline Pressure

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 #!flask/bin/python
 from flask import Flask, json, jsonify
+from datetime import datetime
+import random
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/')
 def index():
     response = app.response_class(
-        response=json.dumps("Hello World"),
+        response=json.dumps("Building AI Systems API"),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route('/api/pipeline-pressure/<room_id>')
+def get_pipeline_pressure(room_id=0):
+    room_data = {
+        'room_id': room_id,
+        'pipeline_pressure': random.randint(1, 5),
+        'date': datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+    }
+    response = app.response_class(
+        response=json.dumps(room_data),
         status=200,
         mimetype='application/json'
     )
